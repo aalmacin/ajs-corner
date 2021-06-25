96e09b6f8dcdf3012be923d99c17edfd730a8de2
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-06-25 00:19:51 -0400

    Add banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ajs-corner';
+  number = '+1-647-207-2216';
 }

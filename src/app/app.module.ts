1f4788aada14c279cdaed28af2a89599de476f7c
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-06-25 23:50:23 -0400

    Rename title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeMenuItemComponent } from './home-menu-item/home-menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeMenuItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-menu-item/home-menu-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-menu-item',
+  templateUrl: './home-menu-item.component.html',
+  styleUrls: ['./home-menu-item.component.scss'],
+})
+export class HomeMenuItemComponent implements OnInit {
+  @Input() flavorText!: string;
+  @Input() heading!: string;
+  @Input() details!: string;
+  @Input() imgSrc!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
